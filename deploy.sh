a981744eb0adbb456f6ba7ad320eab165f05eb97
Nick Cinger <nebojsac@gmail.com>
2016-02-22 11:27:35 +0100

    Update banner 2016

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,7 @@ GITPATH="$CURRENTDIR/" # this file should be in the base of your git repository
 
 # svn config
 SVNPATH="/tmp/$PLUGINSLUG" # path to a temp SVN repo. No trailing slash required and don't add trunk.
-SVNURL="http://plugins.svn.wordpress.org/campayn-email-newsletter-sign-up/" # Remote SVN repo on wordpress.org, with no trailing slash
+SVNURL="https://plugins.svn.wordpress.org/campayn-email-newsletter-sign-up/" # Remote SVN repo on wordpress.org, with no trailing slash
 SVNUSER="nebojsac" # your svn username
 
 
